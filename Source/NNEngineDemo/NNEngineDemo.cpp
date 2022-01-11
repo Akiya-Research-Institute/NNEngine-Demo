7316713dfa7c29febb9a77a6df28bd679f2eb2e7
Kenji Asaba <kasa@akiya-souken.co.jp>
2022-01-11 12:20:21 +0900

    Fixed copyright

--- Source/NNEngineDemo/NNEngineDemo.cpp
+++ Source/NNEngineDemo/NNEngineDemo.cpp
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2021 Akiya Research Institute, Inc. All Rights Reserved
 
 #include "NNEngineDemo.h"
 #include "Modules/ModuleManager.h"
--- Source/NNEngineDemoEditor.Target.cs
+++ Source/NNEngineDemoEditor.Target.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2021 Akiya Research Institute, Inc. All Rights Reserved
 
 using UnrealBuildTool;
 using System.Collections.Generic;
